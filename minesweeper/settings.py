9f8ec8792175a03e18bd9db1876c8b3b9dfa51e9
Syeda Farhat <80599648+Syeda-Farhat@users.noreply.github.com>
2023-05-29 04:23:06 +0500

    Update settings.py

--- minesweeper/settings.py
+++ minesweeper/settings.py
@@ -1,1 +1,7 @@
+# setting the height,width, grid size and cell count into this file
+WIDTH = 1440
+HEIGHT = 720
+GRID_SIZE = 6
+CELL_COUNT = GRID_SIZE ** 2           # ** means power of 2
+MINES_COUNT = (CELL_COUNT) // 4   # for floating using //
 
