4632cc9b140831f0b35a267aa94ccb546bce1815
Syeda Farhat <80599648+Syeda-Farhat@users.noreply.github.com>
2023-05-29 04:23:31 +0500

    Update utils.py

--- minesweeper/utils.py
+++ minesweeper/utils.py
@@ -1,1 +1,13 @@
 
+# file containing the functions for height and width
+import settings
+
+
+def height_prct(percentage):
+    return (settings.HEIGHT/100) * percentage
+
+
+def width_prct(percentage):
+    return (settings.WIDTH / 100) * percentage
+
+
